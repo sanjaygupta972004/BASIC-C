855df761946ef0140dbbedc289f1aa8f19df3cda
sanjay gupta <122673861+sanjaygupta972004@users.noreply.github.com>
2023-01-20 22:26:54 +0530

    push

--- abhi.c
+++ abhi.c
@@ -2,46 +2,19 @@
 #include<conio.h>
 void main()
 {
-int a[3][3],b[3][3],res[3][3];
-int i,j,k;
-printf("enter the elements of matrix a:");
-for(i=0;i<3;i++)
+int a[10];
+int i,n,p=-0;
+printf("enter the elements of a:");
+for(i=0;i<4;i++)
 {
-for(j=0;j<3;j++)
+    scanf("%d",&a[i]); 
+}
+printf("enter the searching element :");
+scanf("%d",&n);
+for(i=0;i<4;i++)
 {
-    scanf("%d",&a[i][j]); 
+    if(n==a[i])
+    printf("number%dis position%d",n,i);
 }
-printf("\n");
-}
-printf("enter the elements matrix:b");
-for(i=0;i<3;i++)
-{
-for(j=0;j<3;j++)
-{
-    scanf("%d",&b[i][j]);
-}
-printf("\n");
-}
-
-for(i=0;i<3;i++)
-for(j=0;j<3;j++)
-{
-    res[i][j]=0;
-    {
-        for (k = 0; k < 3; k++)
-        {
-        res[i][j]+=a[i][k]*b[k][j];
-        }
-    }
-}
-for(i=0;i<3;i++)
-{
-for(j=0;j<3;j++)
-{
-    printf("%d ",res[i][j]);
-}
-printf("\n");
-}
-
 }
 
